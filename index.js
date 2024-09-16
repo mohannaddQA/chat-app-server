1d8dcce22b6fbdf2a59c7ac983c1f8a22d7572a1
mohannaddQA <qa.mohannad@gmail.com>
2024-09-16 21:43:36 +0300

    done

--- index.js
+++ index.js
@@ -8,29 +8,29 @@ const { app, server } = require('./socket/index')
 
 // const app = express()
 const corsOptions = {
-    origin: '*',
+    origin: 'https://chat-app-client-48kp.onrender.com/',
     credentials: true,  // Allow credentials (cookies, authorization headers, etc.)
     methods: 'GET,POST,PUT,DELETE',  // Specify allowed methods
     allowedHeaders: 'Content-Type, Authorization'  // Specify allowed headers
-  };
-  
+};
+
 app.use(cors(corsOptions));  // Apply the CORS middleware
 app.use(express.json())
 app.use(cookiesParser())
 
 const PORT = process.env.PORT || 8080
 
-app.get('/',(request,response)=>{
+app.get('/', (request, response) => {
     response.json({
-        message : "Server running at " + PORT
+        message: "Server running at " + PORT
     })
 })
 
 //api endpoints
-app.use('/api',router)
+app.use('/api', router)
 
-connectDB().then(()=>{
-    server.listen(PORT,()=>{
+connectDB().then(() => {
+    server.listen(PORT, () => {
         console.log("server running at " + PORT)
     })
 })
